4e88b23c640880cd23c6ac766ce88489f7671170
Krystian Wojtas <krystian.wojtas@gmail.com>
2021-08-23 03:53:15 +0200

    lib: reorder functions

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,33 @@ use api::transactions::Type;
 // Crate modules
 pub mod api;
 
+pub fn process(file: &str) -> Result<(), TransactionsProcessorError> {
+    let mut engine = Engine::new();
+
+    let mut rdr = csv::ReaderBuilder::new()
+        .trim(csv::Trim::All)
+        .from_path(file)
+        .map_err(|err| TransactionsProcessorError::CannotReadInputFile(file.to_string(), err))?;
+
+    let mut raw_record = csv::ByteRecord::new();
+    let headers = rdr.byte_headers().map_err(|err| {
+        TransactionsProcessorError::CannotReadInputFileHeaders(file.to_string(), err)
+    })?;
+    let headers = headers.clone();
+
+    while rdr.read_byte_record(&mut raw_record).map_err(|err| {
+        TransactionsProcessorError::CannotReadInputFileRecord(file.to_string(), err)
+    })? {
+        let transaction: Transaction = raw_record.deserialize(Some(&headers)).map_err(|err| {
+            TransactionsProcessorError::CannotDeserializeRecord(file.to_string(), err)
+        })?;
+
+        dispatch(&mut engine, &transaction)?;
+    }
+
+    Ok(())
+}
+
 fn dispatch(
     engine: &mut Engine,
     transaction: &Transaction,
@@ -52,31 +79,4 @@ fn dispatch(
         }
         _ => unimplemented!(),
     }
-}
-
-pub fn process(file: &str) -> Result<(), TransactionsProcessorError> {
-    let mut engine = Engine::new();
-
-    let mut rdr = csv::ReaderBuilder::new()
-        .trim(csv::Trim::All)
-        .from_path(file)
-        .map_err(|err| TransactionsProcessorError::CannotReadInputFile(file.to_string(), err))?;
-
-    let mut raw_record = csv::ByteRecord::new();
-    let headers = rdr.byte_headers().map_err(|err| {
-        TransactionsProcessorError::CannotReadInputFileHeaders(file.to_string(), err)
-    })?;
-    let headers = headers.clone();
-
-    while rdr.read_byte_record(&mut raw_record).map_err(|err| {
-        TransactionsProcessorError::CannotReadInputFileRecord(file.to_string(), err)
-    })? {
-        let transaction: Transaction = raw_record.deserialize(Some(&headers)).map_err(|err| {
-            TransactionsProcessorError::CannotDeserializeRecord(file.to_string(), err)
-        })?;
-
-        dispatch(&mut engine, &transaction)?;
-    }
-
-    Ok(())
 }
