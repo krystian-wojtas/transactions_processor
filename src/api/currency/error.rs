b93c8f02b84f12cd1fbe7bb1b9839fc02995b7ae
Krystian Wojtas <krystian.wojtas@gmail.com>
2021-08-24 16:33:20 +0200

    currency error: thiserror

--- src/api/currency/error.rs
+++ src/api/currency/error.rs
@@ -1,62 +1,29 @@
 //! Common API related to errors in amount
 
 // Standard modules paths
-use std::error;
-use std::fmt;
 use std::num::ParseIntError;
 
-#[derive(Debug, PartialEq)]
+// External paths
+use thiserror::Error;
+
+#[derive(Error, Debug, PartialEq)]
 pub enum CurrencyError {
+    #[error("cannot find decimal part of amount before . character")]
     CannotGetDecimalPart,
+    #[error("cannot parse decimal part of amount: {0}")]
     CannotParseDecimalPart(ParseIntError),
+    #[error("cannot parse fractional part of amount: {0}")]
     CannotParseFractionalPart(ParseIntError),
+    #[error("cannot parse fractional parst of amount as it is too long: {0}")]
     FractionalTooLong(String),
+    #[error("cannot represent amount as value: {0} is out of supported range")]
     DecimalMultipliedByPrecisionOutOfRange(u64),
+    #[error("cannot represent amount as decimal: {0} and fractional: {1} are out of supported range")]
     DecimalAddedFractionalOutOfRange(u64, u64),
+    #[error("cannot represent amount fractional: {0} is out of supported range")]
     FractionalOutOfRange(u64),
+    #[error("cannot add other value as it would be out of supported range")]
     AddingOtherOutOfRange,
+    #[error("cannot substract other value as it would be negative")]
     SubstractingOtherNegative,
-}
-
-// Add empty Error trait
-impl error::Error for CurrencyError {}
-
-fn desc(amount_error: &CurrencyError) -> String {
-    use self::CurrencyError::*;
-    match *amount_error {
-        CannotGetDecimalPart => "cannot find decimal part of amount before . character".to_string(),
-        CannotParseDecimalPart(ref err) => format!("cannot parse decimal part of amount: {}", err),
-        CannotParseFractionalPart(ref err) => {
-            format!("cannot parse fractional part of amount: {}", err)
-        }
-        FractionalTooLong(ref fractional) => format!(
-            "cannot parse fractional parst of amount as it is too long: {}",
-            fractional
-        ),
-        DecimalMultipliedByPrecisionOutOfRange(decimal) => format!(
-            "cannot represent amount as value: {} is out of supported range",
-            decimal
-        ),
-        DecimalAddedFractionalOutOfRange(decimal, fractional) => format!(
-            "cannot represent amount as decimal: {} and fractional: {} are out of supported range",
-            decimal, fractional
-        ),
-        FractionalOutOfRange(fractional) => format!(
-            "cannot represent amount fractional: {} is out of supported range",
-            fractional
-        ),
-        AddingOtherOutOfRange => {
-            format!("cannot add other value as it would be out of supported range",)
-        }
-        SubstractingOtherNegative => {
-            format!("cannot substract other value as it would be negative",)
-        }
-    }
-}
-
-// Implement Display trait
-impl fmt::Display for CurrencyError {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", desc(&self))
-    }
 }
